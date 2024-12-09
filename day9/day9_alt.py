75258f8151ecb5bf29af3aa1af7cfc44b771f9f8
Andrew Reddish <areddish@foosion.net>
2024-12-09 10:30:56 -0500

    Day 9 Alternate Solution

--- day9/day9_alt.py
+++ day9/day9_alt.py
@@ -35,50 +35,30 @@ with open("day9.txt") as file:
     #s = "12345"
     file_list, free_list = get_lists(s)
     
-    # part 1
-    file_list.reverse()
-    current_write_block = free_list.pop(0)
-    current_read_block = file_list.pop(0)
-    while current_write_block[1] < current_read_block[1] and file_list and free_list:
-        # if we have more free space, do copy and update free
-        if current_write_block[2] > current_read_block[2]:            
-            file_list.append([current_read_block[0],current_write_block[1],current_read_block[2]])
-            current_write_block[1] += current_read_block[2]
-            current_write_block[2] -= current_read_block[2]
-            current_read_block = file_list.pop(0)
-        else:
-            # consume full free block
-            file_list.append([current_read_block[0], current_write_block[1], current_write_block[2]])
-            # if we have more in the file block than free, update the size
-            if current_read_block[2] > current_write_block[2]:
-                current_read_block[2] -= current_write_block[2]
-            else:
-                current_read_block = file_list.pop(0)
-            # get next free block
-            current_write_block = free_list.pop(0)
-    # if we have any leftovers, put them back
-    if current_read_block[2] > 0:
-        file_list.append(current_read_block)
-    answer(crc(file_list))
-
+    # part 1, single move
     # part 2, defrag?
-    file_list, free_list = get_lists(s)
-    updated_file_list = []
-    while file_list:
-        # Check each file from the end of the list one by one
-        move_candidate = file_list.pop()
-        #assert all([free_list[a-1][1] < free_list[a][1] for a in range(1,len(free_list))])
-        for free_candidate in free_list:
-            # do not move it after
-            if move_candidate[1] < free_candidate[1]:
-                break
-            # can we move it here?
-            if move_candidate[2] <= free_candidate[2]:
-                # update location to consume free space                
-                move_candidate[1] = free_candidate[1]
-                # update location and lengh of free block
-                free_candidate[2] -= move_candidate[2]
-                free_candidate[1] += move_candidate[2]
-                break
-        updated_file_list.append(move_candidate)
-    answer(crc(updated_file_list))
+    for part in [1,2]:
+        file_list, free_list = get_lists(s)
+        updated_file_list = []
+        while file_list:
+            # Check each file from the end of the list one by one
+            move_candidate = file_list.pop()
+            #assert all([free_list[a-1][1] < free_list[a][1] for a in range(1,len(free_list))])
+            for free_candidate in free_list:
+                # do not move it after
+                if move_candidate[1] < free_candidate[1]:
+                    break
+                # can we move it here?
+                move_size = 1 if part == 1 else move_candidate[2]
+                if move_size <= free_candidate[2]:
+                    if move_candidate[2] > move_size:
+                        file_list.append([move_candidate[0], move_candidate[1], move_candidate[2]-1])
+                        move_candidate[2] = move_size
+                    # update location to consume free space                
+                    move_candidate[1] = free_candidate[1]
+                    # update location and lengh of free block
+                    free_candidate[2] -= move_size
+                    free_candidate[1] += move_size
+                    break
+            updated_file_list.append(move_candidate)
+        answer(crc(updated_file_list))
